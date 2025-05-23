ee746cb6695a396b238bbd2e541899ae5ae8105d
Oukmnut <you@example.com>
2025-05-23 16:36:22 +0700

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+ 
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
@@ -27,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         Schema::defaultStringLength(191);
-        Paginator::useBootstrap();
-        URL::forceHttp();
+        Paginator::useBootstrap(); 
     }
 }
